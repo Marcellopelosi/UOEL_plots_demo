40275bc3902dfab7a7acb808830bc6c4d7e89532
Marcellopelosi <57409544+Marcellopelosi@users.noreply.github.com>
2023-07-04 22:31:28 +0200

    Update feature_eng.py

--- feature_eng.py
+++ feature_eng.py
@@ -1,5 +1,26 @@
+
 import pandas as pd
+import requests
 import numpy as np
+
+def raw_data_to_df(url):
+  f = requests.get(url)
+  data = f.json()
+  df = pd.json_normalize(data)
+  return df
+
+class statsbomb:
+
+  def competitions():
+    return raw_data_to_df("https://raw.githubusercontent.com/statsbomb/open-data/master/data/competitions.json")
+
+  def matches(competition_id, season_id):
+    link = "https://raw.githubusercontent.com/statsbomb/open-data/master/data/matches/" + str(competition_id) + "/" + str(season_id) + ".json"
+    return raw_data_to_df(link)
+
+  def events(match_id):
+    link = "https://raw.githubusercontent.com/statsbomb/open-data/master/data/events/" + str(match_id) + ".json"
+    return raw_data_to_df(link)
 
 
 def all_teams_finder(df_matches):
