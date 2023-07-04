7fc346d243f966078ed26d60458562a4953b630f
Marcellopelosi <57409544+Marcellopelosi@users.noreply.github.com>
2023-07-04 22:23:18 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,26 @@ from shot_map_dashboard_creator import shot_dashboard
 import streamlit as st
 
 
+def raw_data_to_df(url):
+  f = requests.get(url)
+  data = f.json()
+  df = pd.json_normalize(data)
+  return df
+
+class statsbomb:
+
+  def competitions():
+    return raw_data_to_df("https://raw.githubusercontent.com/statsbomb/open-data/master/data/competitions.json")
+
+  def matches(competition_id, season_id):
+    link = "https://raw.githubusercontent.com/statsbomb/open-data/master/data/matches/" + str(competition_id) + "/" + str(season_id) + ".json"
+    return raw_data_to_df(link)
+
+  def events(match_id):
+    link = "https://raw.githubusercontent.com/statsbomb/open-data/master/data/events/" + str(match_id) + ".json"
+    return raw_data_to_df(link)
+
+
 # Retrive the table with all matches listed
 df_matches = find_all_matches() 
 
